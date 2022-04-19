7d23c7cf7ca5c88013b4a3d357a31ab164fa3e76
Igor Mandrik <creation@86@mail.ru>
2022-04-19 23:36:42 +0800

    added test

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,14 +1,11 @@
 package ru.netology.web;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 
 public class CardOrderTest {
 
@@ -36,9 +33,15 @@ public class CardOrderTest {
         }
     }
 
-
     @Test
-    public void shouldOrderCard () {
-
+    public void shouldOrderCard() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Антонов Денис");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79995771202");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector(".paragraph_theme_alfa-on-white")).getText().trim();
+        Assertions.assertEquals(expected, actual);
     }
 }
